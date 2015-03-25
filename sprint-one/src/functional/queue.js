ddbb96b11ca7bfa8d7afab720a691d9206ee0a58
sunshinewyin <sunshine.w.yin@gmail.com>
2015-03-25 15:47:37 -0700

    "Finished through first test of Queue Specific Behavior"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[0];
+    delete storage[0];
+    size--;
+    return result;
   };
 
   someInstance.size = function(){
+    if (size <= 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
