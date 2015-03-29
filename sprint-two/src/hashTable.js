8959b525a9c0af1dd98e0a36e5ee713ef490082d
sunshinewyin <sunshine.w.yin@gmail.com>
2015-03-28 17:29:55 -0700

    "Started. Left off with trying to access the functions in hashTableHelpers.js"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,6 +5,20 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k,v];
+
+  if (this._storage.get(i) === undefined) {
+    var bucket = [];
+    this._storage.set(i,bucket);
+  }
+
+  for (var j = 0; j<bucket.length; j++) {
+    if(bucket[j][0] === k) {
+      bucket[j][1] = v;
+    } else {
+      bucket.push(tuple);
+    }
+  };
 };
 
 HashTable.prototype.retrieve = function(k){
